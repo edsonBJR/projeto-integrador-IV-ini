b9be28d0e56c60be96ee9fb654a7963a16d726d6
Edson Barbosa Junior <edsonbj@arklok025.aquarius.cpqd.com.br>
2022-02-22 20:44:08 -0300

    Iniciando o desenvolvimento da modal - Form cadastro login

--- Site_Estatico/app.js
+++ Site_Estatico/app.js
@@ -5,3 +5,24 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// Modal Itens
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main__btn');
+const closeBtn = document.querySelector('.close-btn');
+
+// Click events
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
+})
